cdcef9b1d43ca8d90948ec20300b5970c2a40d0d
libiyao <50092961+libiyao@users.noreply.github.com>
2021-06-05 22:35:48 -0700

    intern update

--- src/Page/Experience.js
+++ src/Page/Experience.js
@@ -2,11 +2,10 @@ import React, { Component } from "react";
 import styles from "./Experience.module.css";
 import { Grid, Row, Col } from "react-bootstrap";
 import TimeLine from "./TimeLine.js";
-const experience = [{company: "Washington Experimental Mathematics Laboratory", startDate: "Sept, 2020", endDate: "Dec, 2020", position: "Researcher", link: "https://arxiv.org/abs/2012.04625"},
+const experience = [{company: "DocuSign", startDate: "June, 2021", endDate: "Sept, 2021", position: "Data engineering intern"},
+    {company: "Washington Experimental Mathematics Laboratory", startDate: "Sept, 2020", endDate: "Dec, 2020", position: "Researcher", link: "https://arxiv.org/abs/2012.04625"},
 {company: "Local & Yonder", startDate: "Aug, 2020", endDate: "No, 2020", position: "Frontend developer", link: "https://portal.localandyonder.com/"},
                     {company: "Yule technology", startDate: "July, 2020", endDate: "Sept, 2020", position: "Backend developer"},
-
-
                     ];
 class Experience extends Component{
     render(){
